7f3108d555b74217814d06a5a61cc1459c9080a8
20161104604 <1045620410@qq.com>
2018-06-29 14:24:02 +0800

    xiugai5

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  zcp
+//
+//  Created by 20161104604 on 18/6/25.
+//  Copyright © 2018年 20161104604. All rights reserved.
+//
+
+#include <iostream.h>
+struct pingwei   //定义结构类型
+{
+    int num;
+    double score;
+};ping [5]={1,1}{2,2}{3,3}{4,4}{5,5}
+
+void sort(pingwei ping[],int n);
+const int N=5;
+void main()
+{
+    cout<<"请输入裁判的分数\n";
+    cout<<endl;
+    cin>>ping[0].score>>ping[1].score>>ping[2],score>>ping[3].score>>ping[4].score;
+    sort(ping,N);
+    cout<<endl;
+    c
+}
+
